028fac2ee55500f005dae59be598a1548af00ca4
himak <himak@umich.edu>
2024-10-15 20:12:00 -0400

    editing

--- app/ui/acme-logo.tsx
+++ app/ui/acme-logo.tsx
@@ -1,13 +1,19 @@
-import { GlobeAltIcon } from '@heroicons/react/24/outline';
+import Image from 'next/image';
 import { lusitana } from '@/app/ui/fonts';
+
 
 export default function AcmeLogo() {
   return (
     <div
       className={`${lusitana.className} flex flex-row items-center leading-none text-white`}
     >
-      <GlobeAltIcon className="h-12 w-12 rotate-[15deg]" />
-      <p className="text-[44px]">Acme</p>
+      <Image
+          src= "/umsct.png"
+          width={300}
+          height={300}
+          alt="UMSCT Logo"
+    />
+     
     </div>
   );
 }
